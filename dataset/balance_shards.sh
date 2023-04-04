74e99b0e5ffb2e78ed45d009d2e973d69d5a174e
friendshipkim <kwj962004@gmail.com>
2023-04-04 11:47:43 -0400

    skip if target directories already exist

--- dataset/balance_shards.sh
+++ dataset/balance_shards.sh
@@ -2,17 +2,27 @@
 
 for DIR_NAME in $(ls -d /n/tata_ddos_ceph/woojeong/data/output/train_128_20_*)
 do
-    echo "Balance train shards in $DIR_NAME"
-    python balance_shards.py \
-    --dir ${DIR_NAME} \
-    --out-dir ${DIR_NAME}_balanced
+    if [[ -d ${DIR_NAME}_balanced ]] || [[ $DIR_NAME =~ "balanced" ]]
+    then
+        echo "Directory $DIR_NAME exists, skipping" 
+    else
+        echo "Balance train shards in $DIR_NAME"
+        python balance_shards.py \
+            --dir ${DIR_NAME} \
+            --out-dir ${DIR_NAME}_balanced
+    fi
 done
 
 for DIR_NAME in $(ls -d /n/tata_ddos_ceph/woojeong/data/output/test_*)
 do
-    echo "Balance test shards in $DIR_NAME"
-    python balance_shards.py \
-    --dir ${DIR_NAME} \
-    --out-dir ${DIR_NAME}_balanced
+    if [[ -d ${DIR_NAME}_balanced ]] || [[ $DIR_NAME =~ "balanced" ]]
+    then
+        echo "Directory $DIR_NAME exists, skipping" 
+    else
+        echo "Balance test shards in $DIR_NAME"
+        python balance_shards.py \
+            --dir ${DIR_NAME} \
+            --out-dir ${DIR_NAME}_balanced
+    fi
 done
 
--- dataset/copy_train_test_shards.sh
+++ dataset/copy_train_test_shards.sh
@@ -4,20 +4,27 @@ TARGET_BASE_DIR=/n/tata_ddos_ceph/woojeong/data/enwiki_books_128_20_ver2
 
 for SOURCE_DIR in $(ls -d $BASE_DIR/train_128_20_*_balanced)
 do
-    # make new target directory to copy files
     LAST_DIR=$(basename $SOURCE_DIR)
     if [[ $LAST_DIR =~ train_128_20_(.*)_balanced$ ]]; then
         NEW_DIR=${BASH_REMATCH[1]}
     fi
     TARGET_DIR=$TARGET_BASE_DIR/$NEW_DIR
-    # mkdir $TARGET_DIR
 
-    # copy balanced data to the target directory
-    echo "Copying training data from $LAST_DIR to $TARGET_DIR"
-    # cp $SOURCE_DIR/* $TARGET_DIR
+    # check if TARGET_DIR exists
+    if [ -d ${TARGET_DIR} ] 
+    then
+        echo "Directory $TARGET_DIR exists, skipping" 
+    else
+        # make a new target directory
+        mkdir $TARGET_DIR
 
-    # # copy test data to the target directory
-    echo "Copying test data from $BASE_DIR/test_balanced to $TARGET_DIR"
-    # cp $BASE_DIR/test_balanced/* $TARGET_DIR
+        # copy balanced data to the target directory
+        echo "Copying training data from $SOURCE_DIR to $TARGET_DIR"
+        cp $SOURCE_DIR/* $TARGET_DIR
+
+        # # copy test data to the target directory
+        echo "Copying test data from $BASE_DIR/test_balanced to $TARGET_DIR"
+        cp $BASE_DIR/test_balanced/* $TARGET_DIR
+    fi
 done
 

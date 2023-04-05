f8841775c5df399117b3dc8d0747875b820b0504
friendshipkim <kwj962004@gmail.com>
2023-04-05 17:25:00 -0400

    cache total lengths

--- dataset/balance_shards.py
+++ dataset/balance_shards.py
@@ -2,6 +2,7 @@ import argparse
 import glob
 import os
 import h5py
+import pickle
 import numpy as np
 from tqdm import tqdm
 
@@ -28,8 +29,8 @@ def get_partial_numpy(lst_of_npy, num_rows):
     curr_shape = sum([x.shape[0] for x in lst_of_npy])
     # print(prev_shape, curr_shape, sum([x.shape[0] for x in result]), num_rows)
 
-    assert prev_shape - curr_shape == num_rows
-    assert sum([x.shape[0] for x in result]) == num_rows
+    assert prev_shape - curr_shape == num_rows, f"num_rows: {num_rows}, prev_shape - curr_shape: {prev_shape - curr_shape} "
+    assert sum([x.shape[0] for x in result]) == num_rows, f"num_rows: {num_rows}, result: {sum([x.shape[0] for x in result])}"
     return np.vstack(result)
 
 
@@ -44,18 +45,27 @@ def find_num_samples_in_shard(files):
     return lengths
 
 
-def balance(files, out_dir):
-    lengths = find_num_samples_in_shard(files)
+def balance(files, out_dir, lengths_file=None):
+    if lengths_file is None:
+        lengths = find_num_samples_in_shard(files)
+        # # Save as pickle file (optional)
+        # with open('total_lengths.pkl', 'wb') as f:
+        #     pickle.dump(lengths, f)
+    else:
+        with open(lengths_file, 'rb') as f:
+            lengths = pickle.load(f)
+        
     if len(lengths) == 0:
         return
     average_length = sum([x[0] for x in lengths]) // len(lengths)
     print("*" * 72)
-    print(sum([x[0] for x in lengths]))
-    print(average_length)
-    print(len(lengths))
+    print("total length:", sum([x[0] for x in lengths]))
+    print("average length:", average_length)
+    print("file count:", len(lengths))
+    
     ## We have to re-distribute the shards so that each shard contains equal number of samples.
     ## Since we are operating on tokenised data, re-distributing should not alter the perfornance (IID assumption)
-
+    
     file_content = {}
     print("Re sharding data into equal blocks")
     for length, file in tqdm(lengths):
@@ -73,7 +83,7 @@ def balance(files, out_dir):
         fout = h5py.File(
             os.path.join(out_dir, os.path.basename(file)), "w", libver="latest"
         )
-
+        
         for key in keys:
             fout.create_dataset(
                 key,
@@ -86,18 +96,20 @@ def balance(files, out_dir):
         fout.close()
 
 
-def main(files, out_dir):
+def main(files, out_dir, train_lengths, test_lengths):
     train_files = [file for file in files if "train_shard" in file]
     test_files = [file for file in files if "test_shard" in file]
-    balance(train_files, out_dir)
-    balance(test_files, out_dir)
+    balance(train_files, out_dir, train_lengths)
+    balance(test_files, out_dir, test_lengths)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--dir")
     parser.add_argument("--out-dir")
+    parser.add_argument("--train-lengths", type=str, required=False, default=None)
+    parser.add_argument("--test-lengths", type=str, required=False, default=None)
     args = parser.parse_args()
     os.makedirs(args.out_dir, exist_ok=True)
     files = glob.glob(os.path.join(args.dir, "*"))
-    main(files, args.out_dir)
+    main(files, args.out_dir, args.train_lengths, args.test_lengths)

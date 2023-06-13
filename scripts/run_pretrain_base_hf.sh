61a408980ccdb5fac9359bcb539ad473f6a3bc2a
wkim-ext <wk247@cornell.edu>
2023-06-13 16:00:19 -0400

    add base training scripts

---
+++ scripts/run_pretrain_base_hf.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+eval "$(conda shell.bash hook)"
+conda activate pretrain
+export WANDB_MODE=online
+export CUDA_VISIBLE_DEVICES=0,1
+export PYTORCH_CUDA_ALLOC_CONF=max_split_size_mb:512
+export NCCL_P2P_LEVEL=NVL
+deepspeed --num_gpus 2 --master_port 29501 run_pretraining.py \
+  --model_type bert-mlm --tokenizer_name bert-large-uncased \
+  --hidden_act gelu \
+  --hidden_size 768 \
+  --num_hidden_layers 12 \
+  --num_attention_heads 12 \
+  --intermediate_size 3072 \
+  --hidden_dropout_prob 0.1 \
+  --attention_probs_dropout_prob 0.1 \
+  --encoder_ln_mode post-ln \
+  --lr 2e-4 \
+  --train_batch_size 512 \
+  --train_micro_batch_size_per_gpu 256 \
+  --lr_schedule step \
+  --curve linear \
+  --gradient_clipping 0.0 \
+  --optimizer_type adamw \
+  --weight_decay 0.01 \
+  --adam_beta1 0.9 \
+  --adam_beta2 0.98 \
+  --adam_eps 1e-6 \
+  --max_steps 200000 \
+  --num_warmup_steps 5000 \
+  --print_steps 100 \
+  --num_epochs_between_checkpoints 100 \
+  --dataset_path /home/wk247/data/enwiki_books_128_20_ver2/total/ \
+  --output_dir /home/wk247/saved_models/pretrain/ \
+  --job_name base-hf \
+  --current_run_id total-bsz512-200ksteps-5val-lr2e-4 \
+  --project_name budget-bert-pretraining \
+  --validation_epochs 3 \
+  --validation_epochs_begin 1 \
+  --validation_epochs_end 1 \
+  --validation_begin_proportion 0.05 \
+  --validation_end_proportion 0.01 \
+  --validation_micro_batch 128 \
+  --validation_shards 5 \
+  --deepspeed \
+  --data_loader_type dist \
+  --do_validation \
+  --seed 33 \
+  --fp16 \
+  --hf_architecture
--- scripts/run_pretrain_halfbase_hf.sh
+++ scripts/run_pretrain_halfbase_hf.sh
@@ -1,19 +1,22 @@
-# Script to pretrain a half-large model
-# Train for 10k steps with 1 gpu
-
+#!/bin/bash
+eval "$(conda shell.bash hook)"
+conda activate pretrain
 export WANDB_MODE=online
-deepspeed --include localhost:2 --master_port 29502 run_pretraining.py \
+export CUDA_VISIBLE_DEVICES=0,1
+export PYTORCH_CUDA_ALLOC_CONF=max_split_size_mb:512
+export NCCL_P2P_LEVEL=NVL
+deepspeed --num_gpus 2 --master_port 29500 run_pretraining.py \
   --model_type bert-mlm --tokenizer_name bert-large-uncased \
   --hidden_act gelu \
-  --hidden_size 768 \
+  --hidden_size 384 \
   --num_hidden_layers 12 \
-  --num_attention_heads 12 \
-  --intermediate_size 2048 \
+  --num_attention_heads 6 \
+  --intermediate_size 1536 \
   --hidden_dropout_prob 0.1 \
   --attention_probs_dropout_prob 0.1 \
   --encoder_ln_mode post-ln \
   --lr 2e-4 \
-  --train_batch_size 256 \
+  --train_batch_size 512 \
   --train_micro_batch_size_per_gpu 256 \
   --lr_schedule step \
   --curve linear \
@@ -23,14 +26,14 @@ deepspeed --include localhost:2 --master_port 29502 run_pretraining.py \
   --adam_beta1 0.9 \
   --adam_beta2 0.98 \
   --adam_eps 1e-6 \
-  --max_steps 400000 \
-  --num_warmup_steps 10000 \
+  --max_steps 200000 \
+  --num_warmup_steps 5000 \
   --print_steps 100 \
   --num_epochs_between_checkpoints 100 \
-  --dataset_path /n/tata_ddos_ceph/woojeong/data/enwiki_books_128_20_ver2/total \
-  --output_dir /n/tata_ddos_ceph/woojeong/saved_models/pretrain/ \
-  --job_name base-ligo \
-  --current_run_id total-bsz256-400ksteps-5val \
+  --dataset_path /home/wk247/data/enwiki_books_128_20_ver2/total/ \
+  --output_dir /home/wk247/saved_models/pretrain/ \
+  --job_name base-hf \
+  --current_run_id total-bsz512-400ksteps-5val-lr2e-4 \
   --project_name budget-bert-pretraining \
   --validation_epochs 3 \
   --validation_epochs_begin 1 \
@@ -42,7 +45,6 @@ deepspeed --include localhost:2 --master_port 29502 run_pretraining.py \
   --deepspeed \
   --data_loader_type dist \
   --do_validation \
-  --seed 42 \
+  --seed 33 \
   --fp16 \
-  --load_tokenizer_locally \
   --hf_architecture

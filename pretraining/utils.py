2cfa23ce2c4ecfdb49a9401137ab5c0a44656571
wkim-ext <wk247@cornell.edu>
2023-06-21 11:31:48 -0400

    add count parameters

--- pretraining/utils.py
+++ pretraining/utils.py
@@ -116,3 +116,7 @@ def set_seeds(seed):
     np.random.seed(seed)
     torch.manual_seed(seed)
     torch.cuda.manual_seed_all(seed)
+
+
+def count_parameters(model):
+    return sum(p.numel() for p in model.parameters() if p.requires_grad)
